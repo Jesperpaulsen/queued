7867069bc75a30e755cf790c8b7c97bbd82fe586
Jesper Paulsen <jesper@jesper.no>
2020-12-27 15:54:16 +0100

    feat: initalized app with web support

---
+++ android/app/src/main/kotlin/com/example/queued/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.queued
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/no/jesper/queued/MainActivity.kt
@@ -0,0 +1,6 @@
+package no.jesper.queued
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
